1ec200c444b2c10f98d544763bc65696c4338fa5
Sam Stoelinga <sammiestoel@gmail.com>
2019-12-30 13:58:36 -0800

    Update docker-veth.sh script

--- docker-veth.sh
+++ docker-veth.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-set -xe
+
+usage () {
+    cat <<HELP_USAGE
+
+    $0  Show the veth interface associated of containers
+
+    Examples:
+
+    $0 # shows all containers
+    $0 --filter "name=nginx" # filter containers
+
+    See docker ps --help docs for more info on filters
+
+HELP_USAGE
+}
+
+case "$1" in
+    -h|--help)
+    usage
+    exit 0
+    ;;
+    *)
+    ;;
+esac
 
 veth=""
 
@@ -10,13 +33,16 @@ get_veth () {
     # This function expects docker container ID as the first argument
     veth=""
     networkmode=$(docker inspect -f "{{.HostConfig.NetworkMode}}" $1)
-    echo $networkmode
     if [ "$networkmode" == "host" ]; then
         veth="host"
     else
         pid=$(docker inspect --format '{{.State.Pid}}' "$1")
         ifindex=$(nsenter -t $pid -n ip link | sed -n -e 's/.*eth0@if\([0-9]*\):.*/\1/p')
-        veth=$(ip -o link | grep ^$ifindex | sed -n -e 's/.*\(veth[[:alnum:]]*@if[[:digit:]]*\).*/\1/p')
+        if [ -z "$ifindex" ]; then
+            veth="not found"
+        else
+            veth=$(ip -o link | grep ^$ifindex | sed -n -e 's/.*\(veth[[:alnum:]]*@if[[:digit:]]*\).*/\1/p')
+        fi
     fi
 }
 
